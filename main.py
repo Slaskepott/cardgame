4419a75bab8b32277dec3bb352da8c7b7294af34
Henrik Monteagle Browne <henrikmb@ulrik.uio.no>
2025-02-12 22:04:15 +0100

    minor changes to play_card function

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Card:
 def generate_deck():
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     suits = ["Fire", "Air", "Earth", "Water"]
-    return [Card(rank, suit) for rank in ranks for suit in suits]
+    return [Card(rank, suit) for rank in ranks for suit in suits] * 10
 
 class Player:
     def __init__(self, name: str):
