bb69e202e186c866aa8471ba00ea6d1b4acfeac6
Henrik Monteagle Browne <henrikmb@ulrik.uio.no>
2025-02-13 20:31:09 +0100

    Separated card, game, player from main

--- game.py
+++ game.py
@@ -106,7 +106,7 @@ class Game:
     def generate_deck(self):
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         suits = ["Fire", "Air", "Earth", "Water"]
-        return [Card(rank, suit) for rank in ranks for suit in suits]
+        return [Card(rank, suit) for rank in ranks for suit in suits] * 10
 
     def calculate_damage(self, cards):
         """Evaluates a hand and returns damage and hand type based on poker multipliers."""
